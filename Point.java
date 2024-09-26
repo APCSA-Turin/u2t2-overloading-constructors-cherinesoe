dd192b3bd6afc78aa7360ac4d3d14a40793010ed
cherinesoe <148895697+cherinesoe@users.noreply.github.com>
2024-09-26 12:21:10 +0000

    point

--- Point.java
+++ Point.java
@@ -16,4 +16,44 @@ public class Point {
         x = 0;
         y = 0;
     }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int newX) {
+        x = newX;
+    }
+
+    public void setY(int newY) {
+        y = newY;
+    }
+
+    public String coordinate() {
+        return "(" + x + "," + y + ")";
+    }
+
+    public String quadrant() {
+        if (x == 0 && y == 0) {
+            return "origin";
+        } else if (x == 0 || y == 0) {
+            return "on an axis";
+        } else if (x > 0) {
+            if (y > 0) {
+                return "I";
+            } else {
+                return "IV";
+            }
+        } else {
+            if (y > 0) {
+                return "II";
+            } else {
+                return "III";
+            }
+        }
+    }
 }
